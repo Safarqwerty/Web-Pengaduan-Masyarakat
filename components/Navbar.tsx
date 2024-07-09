8ba8898c31d76cf6f60e2f1f32eaae757a1fedff
Safarqwerty <safar.alam79@gmail.com>
2024-07-09 23:08:10 +0800

    navbar mobile perbaikan

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,47 @@
+"use client";
+
+import { useState, useEffect, useRef } from 'react';
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./Button";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+    <nav className="flexBetween max-container padding-container relative z-30 py-5" ref={menuRef}>
       <Link href="/">
         <Image src="/pengaduan.svg" alt="logo" width={150} height={50} />
       </Link>
-      <ul className="hidden h-full gap-12 lg:flex">
+      <ul className={`lg:flex items-center gap-12 ${menuOpen ? 'block' : 'hidden'} lg:block absolute top-full left-0 w-full bg-white lg:bg-transparent lg:static lg:w-auto`}>
         {NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
-          >
-            {link.label}
-          </Link>
+          <li key={link.key} className="lg:inline-block block" onClick={() => setMenuOpen(false)}>
+            <Link
+              href={link.href}
+              className="block lg:inline-block regular-16 text-gray-50 lg:text-gray-50 py-2 px-6 lg:py-0 lg:px-0 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
         <div className="lg:flexCenter hidden">
           <Button
@@ -35,6 +59,7 @@ const Navbar = () => {
         height={32}
         width={32}
         className="inline-block cursor-pointer lg:hidden"
+        onClick={toggleMenu}
       />
     </nav>
   );
